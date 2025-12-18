5ce756c96b1116d21aad74ecf5451956c0ea1aa7
chenxinye <cclcquant@yahoo.com>
2025-12-18 21:10:21 +0100

    init commit

---
+++ build/CMakeFiles/hss_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hss_test.dir/src/main.cpp.o"
+  "CMakeFiles/hss_test.dir/src/main.cpp.o.d"
+  "hss_test"
+  "hss_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hss_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
